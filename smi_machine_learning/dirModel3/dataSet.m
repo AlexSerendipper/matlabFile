f990f593d6d7016941b69976118892e4e7ff0b4d
alex <642671525@qq.com>
2023-06-13 09:51:10 +0800

    my local changes

--- smi_machine_learning/dirModel3/dataSet.m
+++ smi_machine_learning/dirModel3/dataSet.m
@@ -5,7 +5,7 @@ close all;
 % 数据集
 fringeData = [];
 
-for C=0.05:0.01:0.2
+for C=0.02:0.01:3
     for alpha=4:0.1:6
         [data] = SMI_API_FringeData(C,alpha);
         fringeData = [fringeData;data];
